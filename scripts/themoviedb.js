3ea6ecf9454a1113ee46c78e58210c5405bf5068
Bart Langelaan <langelaanbart@gmail.com>
2015-04-13 16:29:54 +0200

    TheMovieDB Episode errors

--- scripts/themoviedb.js
+++ scripts/themoviedb.js
@@ -27,12 +27,12 @@ angular.module("themoviedb", []).directive('mbThemoviedbCast', function(){
 						var url = 'http://api.themoviedb.org/3/movie/'+ data.movie_results[0].id +'/credits?api_key='+ tmdbKey;
 					}
 					else {
-						if(!data.episode_results.length){
+						if(!data.tv_episode_results.length){
 							$scope.cast = {error: "Episode not found"};
 							console.warn("[WARNING] [Themoviedb] Episode not found");
 							return;
 						}
-						var url = 'http://api.themoviedb.org/3/tv/'+ data.epsiode_results[0].show_id +'/season/'+ watching.season +'/episode/'+ watching.number +'/credits?api_key='+ tmdbKey;
+						var url = 'http://api.themoviedb.org/3/tv/'+ data.tv_episode_results[0].show_id +'/season/'+ watching.episode.season +'/episode/'+ watching.episode.number +'/credits?api_key='+ tmdbKey;
 					}
 
 					promise = $http({
