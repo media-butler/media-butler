da99314283e86ab76035c14b0e520a5bb8b859d9
Arjo <arjo@weirdwater.net>
2015-04-13 17:55:03 +0200

    Changed cast widget logic, added imdb link

--- scripts/themoviedb.js
+++ scripts/themoviedb.js
@@ -4,44 +4,80 @@ angular.module("themoviedb", []).directive('mbThemoviedbCast', function(){
         controller: function($scope, $http){
 			
 			document.addEventListener('watchingChange', function(){
-				
+				// Set loading status to true
 				$scope.cast = {loading: true};
 				var watching = $scope.trakt.watching;
 				var tmdbKey = 'dd6ed86bdb03617ceff53e847e651298';
 				
-				var imdb = watching.type == "episode" ? watching.episode.ids.imdb : watching.movie.ids.imdb;
-				
-				$http({
-					url: 'http://api.themoviedb.org/3/find/'+ imdb +'?external_source=imdb_id&api_key='+ tmdbKey
+				// Prepare url for XHR
+				if(watching.type == 'movie') {
+					var url = 'http://api.themoviedb.org/3/movie/'+ watching.movie.ids.tmdb // The movie's TMDB id
+								+'/credits?api_key='+ tmdbKey;								// API key for TMDB
+				}
+				else {
+					var url = 'http://api.themoviedb.org/3/tv/'+ watching.show.ids.tmdb     // The show's TMDB id
+								+'/season/'+ watching.episode.season 						// The episode's season id
+								+'/episode/'+ watching.episode.number 						// The episode's number
+								+'/credits?api_key='+ tmdbKey;								// API key for TMDB
+				}
+
+				// Call TMDB for item info
+				// In case of success the scope is replaced by the returned data
+				// In case of an error the scope is replaced by the error data
+				var promise = $http({
+					url: url
 				}).success(function(data){
 					console.log(data);
-					if(watching.type == 'movie') {
-						if(!data.movie_results.length){
-							$scope.cast = {error: "Movie not found"};
-							console.warn("[WARNING] [Themoviedb] Movie not found");
-							return;
-						}
-						var url = 'http://api.themoviedb.org/3/movie/'+ data.movie_results[0].id +'/credits?api_key='+ tmdbKey;
-					}
-					else {
-						if(!data.tv_episode_results.length){
-							$scope.cast = {error: "Episode not found"};
-							console.warn("[WARNING] [Themoviedb] Episode not found");
-							return;
-						}
-						var url = 'http://api.themoviedb.org/3/tv/'+ data.tv_episode_results[0].show_id +'/season/'+ watching.episode.season +'/episode/'+ watching.episode.number +'/credits?api_key='+ tmdbKey;
-					}
-
-					promise = $http({
-						url: url
-					}).success(function(data){
-						$scope.cast = data;
-					}).error(function(data){
-						$scope.cast = {error: data};
+					// Get imdb id for each actor
+					$.each(data.cast, function(i){
+						// Isolate actor
+						var person = this;
+						// Request actor's info
+						promise = $http({
+							url: 'http://api.themoviedb.org/3/person/'+ person.id
+									+ '?api_key='+ tmdbKey;
+						});
+						// Add retrieved imdb id to actor
+						promise.success(function(data){
+							person.imdb = data.imdb_id;
+						})
+						// Make imdb id empty
+						.error(function(data){
+							person.imdb = null;
+						})
+						// Add actor to the people array
+						.finally(function(){
+							$scope.cast.people.push(person);
+						});
 					});
+					// do the same for guest stars
+					if(watching.type == 'episode') {
+						$.each(data.guest_stars, function(i){
+						// Isolate actor
+						var person = this;
+						// Request actor's info
+						promise = $http({
+							url: 'http://api.themoviedb.org/3/person/'+ person.id
+									+ '?api_key='+ tmdbKey;
+						});
+						// Add retrieved imdb id to actor
+						promise.success(function(data){
+							person.imdb = data.imdb_id;
+						})
+						// Make imdb id empty
+						.error(function(data){
+							person.imdb = null;
+						})
+						// Add actor to the people array
+						.finally(function(){
+							$scope.cast.people.push(person);
+						});
+					});
+					}
 				}).error(function(data){
 					$scope.cast = {error: data};
 				});
+
 				
 			});
         }
